ef37dd68e39b5d58ee084969784bb5da7b058969
Влад Черный <stalkervaultboy@gmail.com>
2022-03-25 01:08:47 +0400

    Started lab, added Tree struct

--- ADS_Labs_2S/Set.h
+++ ADS_Labs_2S/Set.h
@@ -2,12 +2,17 @@
 #include <iostream>
 
 struct Tree {
-
+	int key;
+	Tree* left;
+	Tree* right;
+	unsigned height;
 };
 
 class Set {
-
+	Tree* root;
 public:
+	Set();
+	~Set();
 	void Print() const;
 	bool Insert(int key);
 	bool Find(int key) const;
