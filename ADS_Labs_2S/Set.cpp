b4d4a371a5bcb9c3fa65c029dafd3e4dd0811c81
Влад Черный <stalkervaultboy@gmail.com>
2022-03-29 02:03:19 +0400

    Complete realization for base methods

--- ADS_Labs_2S/Set.cpp
+++ ADS_Labs_2S/Set.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include "Set.h"
 
+using namespace std;
+
 Tree* Set::_Insert(Tree* root, int key)
 {
 
@@ -58,26 +60,59 @@ Set::Set()
 
 Set::~Set()
 {
-	//Delete_tree(root);
+	_Delete_tree(root);
 	root = nullptr;
 }
 
 void Set::Print() const
 {
-
+	if(root)
+	{
+		_Print(root, 0);
+	}
+	else
+	{
+		cout << "Tree is empty";
+	}
 }
 
 bool Set::Insert(int key)
 {
-
+	root = _Insert(root, key);
+	return true;
 }
 
 bool Set::Find(int key) const
 {
-
+	if (!root) 
+	{ 
+		cout << "Tree is empty";
+		return false; 
+	}
+	Tree* temp = root;
+	while (temp != nullptr)
+	{
+		if (key < temp->key)
+		{
+			temp = temp->left;
+		}
+		else if (key > temp->key)
+		{
+			temp = temp->right;
+		}
+		else if (key == temp->key)
+		{
+			return true;
+		}
+		else
+		{
+			throw "The element doesn't exist";
+		}
+	}
 }
 
 bool Set::Erase(int key)
 {
-
+	root = _Erase_elem(root, key);
+	return true;
 }
