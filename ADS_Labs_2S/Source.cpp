71e166d67e48d1600689a1d67891d83ef382d5cf
Влад Черный <stalkervaultboy@gmail.com>
2022-04-26 09:14:33 +0400

    Completed Shell Sort

--- ADS_Labs_2S/Source.cpp
+++ ADS_Labs_2S/Source.cpp
@@ -30,10 +30,32 @@ Stats Bubble_Sort(vector<int> &value)
 	return res;
 }
 
+Stats Shell_Sort(vector<int>& value)
+{
+	Stats res;
+	for (size_t i = value.size() / 2; i > 0; i /= 2)
+	{
+		for (size_t j = i; j < value.size(); ++j)
+		{
+			res.compare_count++;
+			for (int t = j - i; t >= 0 && value[t] > value[t + i]; t -= i)
+			{
+				int tmp = value[t];
+				value[t] = value[t + i];
+				value[t + i] = tmp;
+				res.copy_count++;
+			}
+		}
+	}
+	cout << "Compare count: " << res.compare_count << endl;
+	cout << "Copy count: " << res.copy_count << endl;
+	return res;
+}
+
 int main()
 {
 	Stats s;
-	vector<int> v1 = { 17,1,9,8,5,4,1,5,26,88, 7 };
+	vector<int> v1 = { 17,1,9,8,5,4,1,5,26,88,7 };
 	for (auto i = v1.begin(); i != v1.end(); ++i)
 	{
 		cout << *i << " ";
@@ -41,6 +63,18 @@ int main()
 	cout << endl;
 	s = Bubble_Sort(v1);
 	for (auto i = v1.begin(); i != v1.end(); ++i)
+	{
+		cout << *i << " ";
+	}
+	cout << endl << endl;
+	vector<int> v2 = { 17,1,9,8,5,4,1,5,26,88,7 };
+	for (auto i = v2.begin(); i != v2.end(); ++i)
+	{
+		cout << *i << " ";
+	}
+	cout << endl;
+	s = Shell_Sort(v2);
+	for (auto i = v2.begin(); i != v2.end(); ++i)
 	{
 		cout << *i << " ";
 	}
